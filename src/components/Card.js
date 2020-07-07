5aa28f3db22ef6e0daf88af872156627ae8522b8
Ward Peeters <ward@coding-tech.com>
2020-07-07 09:59:32 +0200

    setup eager

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,33 +5,34 @@ import Img from 'gatsby-image'
 
 const Post = styled.li`
   position: relative;
-  border: 1px solid ${props => props.theme.colors.secondary};
+  border: 1px solid ${(props) => props.theme.colors.secondary};
   border-radius: 2px;
   margin: 0 0 1em 0;
   width: 100%;
   transition: background 0.2s;
-  @media screen and (min-width: ${props => props.theme.responsive.small}) {
-    flex: ${props => (props.featured ? '0 0 100%' : '0 0 49%')};
+  @media screen and (min-width: ${(props) => props.theme.responsive.small}) {
+    flex: ${(props) => (props.featured ? '0 0 100%' : '0 0 49%')};
     margin: 0 0 2vw 0;
   }
-  @media screen and (min-width: ${props => props.theme.responsive.medium}) {
-    flex: ${props => (props.featured ? '0 0 100%' : '0 0 32%')};
+  @media screen and (min-width: ${(props) => props.theme.responsive.medium}) {
+    flex: ${(props) => (props.featured ? '0 0 100%' : '0 0 32%')};
   }
   &:hover {
-    background: ${props => props.theme.colors.tertiary};
+    background: ${(props) => props.theme.colors.tertiary};
   }
   a {
     display: flex;
     flex-flow: column;
     height: 100%;
     width: 100%;
-    color: ${props => props.theme.colors.text};
+    color: ${(props) => props.theme.colors.text};
     text-decoration: none;
     .gatsby-image-wrapper {
       height: 0;
       padding-bottom: 60%;
-      @media screen and (min-width: ${props => props.theme.responsive.small}) {
-        padding-bottom: ${props => (props.featured ? '40%' : '60%')};
+      @media screen and (min-width: ${(props) =>
+          props.theme.responsive.small}) {
+        padding-bottom: ${(props) => (props.featured ? '40%' : '60%')};
       }
     }
   }
@@ -64,13 +65,25 @@ const Excerpt = styled.p`
   line-height: 1.6;
 `
 
-const Card = ({ slug, heroImage, title, publishDate, body, ...props }) => {
+const Card = ({
+  slug,
+  heroImage,
+  title,
+  publishDate,
+  body,
+  featured,
+  ...props
+}) => {
   return (
     <>
       {heroImage && body && (
-        <Post featured={props.featured}>
+        <Post featured={featured}>
           <Link to={`${props.basePath}/${slug}/`}>
-            <StyledImg fluid={heroImage.fluid} backgroundColor={'#eeeeee'} />
+            <StyledImg
+              fluid={heroImage.fluid}
+              backgroundColor={'#eeeeee'}
+              loading={featured ? 'eager' : 'lazy'}
+            />
             <Title>{title}</Title>
             <Date>{publishDate}</Date>
             <ReadingTime>
